bac597469fa313cc306578651b74725e4723694b
Jared Kells <jkells@gmail.com>
2013-10-23 10:22:57 +1100

    Sample project

---
+++ Xamarin.Android.Crashlytics.Sample/Activity1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Xamarin.Android.Crashlytics.Sample
+{
+    [Activity(Label = "Xamarin.Android.Crashlytics.Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            var button1 = FindViewById<Button>(Resource.Id.Crash1);
+            var button2 = FindViewById<Button>(Resource.Id.Crash2);
+
+            button1.Click += (sender, args) =>
+                             {
+                                 throw new InvalidOperationException("This is a .NET exception!");
+                             };
+
+            // Should crash in Java
+            button2.Click += (sender, args) => SetContentView(888888);
+        }
+    }
+}
+
--- Xamarin.Android.Crashlytics.Sample/Properties/AssemblyInfo.cs
+++ Xamarin.Android.Crashlytics.Sample/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.Android.Crashlytics.Binding")]
+[assembly: AssemblyTitle("Xamarin.Android.Crashlytics.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.Android.Crashlytics.Binding")]
+[assembly: AssemblyProduct("Xamarin.Android.Crashlytics.Sample")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,10 +25,10 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1.1")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-  
