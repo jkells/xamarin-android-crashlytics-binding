97e147ecd4025284d3aee9941d15d7ea5856f7c2
Jared Kells <jkells@gmail.com>
2014-04-28 14:09:11 +1000

    Fix some binding errors with the latest version

--- Xamarin.Android.Crashlytics.Binding/Additions/CrashReporting.cs
+++ Xamarin.Android.Crashlytics.Binding/Additions/CrashReporting.cs
@@ -2,30 +2,36 @@
 using Android.Content;
 using Android.Runtime;
 using Java.Lang;
-
 using JavaException = Java.Lang.Exception;
+
 // ReSharper disable once CheckNamespace
+
 namespace Xamarin.Android.Crashlytics
 {
     public class CrashReporting
     {
-        private static void AndroidEnvironmentOnUnhandledExceptionRaiser(RaiseThrowableEventArgs eventArgs, bool callJavaDefaultUncaughtExceptionHandler)
+        private static void AndroidEnvironmentOnUnhandledExceptionRaiser(RaiseThrowableEventArgs eventArgs,
+            bool callJavaDefaultUncaughtExceptionHandler)
         {
             JavaException exception = MonoException.Create(eventArgs.Exception);
 
             if (callJavaDefaultUncaughtExceptionHandler && Thread.DefaultUncaughtExceptionHandler != null)
                 Thread.DefaultUncaughtExceptionHandler.UncaughtException(Thread.CurrentThread(), exception);
             else
-                Crashlytics.LogException(exception);            
+                Crashlytics.LogException(exception);
         }
-        
+
         public static void StartWithMonoHook(Context context, bool callJavaDefaultUncaughtExceptionHandler)
         {
-            if(context == null)
+            if (context == null)
                 throw new ArgumentNullException("context");
 
             Crashlytics.Start(context);
-            AndroidEnvironment.UnhandledExceptionRaiser += (sender, args) => { AndroidEnvironmentOnUnhandledExceptionRaiser(args, callJavaDefaultUncaughtExceptionHandler); };
+            AndroidEnvironment.UnhandledExceptionRaiser +=
+                (sender, args) =>
+                {
+                    AndroidEnvironmentOnUnhandledExceptionRaiser(args, callJavaDefaultUncaughtExceptionHandler);
+                };
         }
     }
 }
